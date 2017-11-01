5789115b6ef2bae3ad1a8f309471b4874d085a60
Alumno ITESM Toluca <Alumnos@imac-de-tecnologia.local>
2017-11-01 07:41:43 -0600

    Initial Commit

---
+++ PeliculasApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PeliculasApp
+//
+//  Created by Alumno ITESM Toluca on 01/11/17.
+//  Copyright © 2017 Alumno ITESM Toluca. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
